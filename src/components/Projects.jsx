1a48c6772b850b32a83ca76f6a2395802d656937
Edoardo Giambuzzi <edoardo.giambuzzi@gmail.com>
2024-02-25 10:06:21 +0100

    first commit

---
+++ src/components/Projects.jsx
@@ -0,0 +1,39 @@
+export const projects = [
+  {
+    name: "Berlin Warrior",
+    logoUrl:
+      "https://res.cloudinary.com/dgwvbd9ki/image/upload/v1708599344/portfolio/5-removebg-preview_whygfs.png",
+    videoUrl: "ciaone",
+    description: "A Berlin game.",
+  },
+  {
+    name: "serenitas",
+    logoUrl:
+      "https://res.cloudinary.com/dgwvbd9ki/image/upload/v1708599342/portfolio/2-removebg-preview_swhv6r.png",
+    videoUrl: "",
+    description: "A timer.",
+  },
+  {
+    name: "Vox Stoica API",
+    logoUrl:
+      "https://res.cloudinary.com/dgwvbd9ki/image/upload/v1708599341/portfolio/1-removebg-preview_jtzf52.png",
+    videoUrl:
+      "https://res.cloudinary.com/dgwvbd9ki/video/upload/v1708601061/project-sample-videos/vox-stoica-api_f7nojd.mov",
+    description: "Voices from the past.",
+  },
+  {
+    name: "Tiny Treats",
+    logoUrl:
+      "https://res.cloudinary.com/dgwvbd9ki/image/upload/v1708599343/portfolio/3-removebg-preview_1_drfpik.png",
+    videoUrl: "ciaone",
+    description: "A platform to buy gifts in.",
+  },
+  {
+    name: "Spellworth Farm",
+    logoUrl:
+      "https://res.cloudinary.com/dgwvbd9ki/image/upload/v1708599342/portfolio/4-removebg-preview_pc4gsq.png",
+    videoUrl:
+      "https://res.cloudinary.com/dgwvbd9ki/video/upload/v1708600977/project-sample-videos/spellworth_bp7wbg.mov",
+    description: "A farm.",
+  },
+];
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,58 @@
+import { projects } from "../components/Projects";
+
+export const Homepage = () => {
+  return (
+    <>
+      <div className="homepage-container">
+        <div className="top-section">
+          <div className="header-top-section-text">
+            <h1>Indie Web Developer</h1>
+            <p>
+              {" "}
+              I'm Edoardo Giambuzzi. I'm an Indie Web Developer, and I enjoy
+              creating apps that increase your productivity and peace of mind.
+            </p>
+            <div className="header-top-section-text-links">
+              <h5>Contact</h5>
+              <h5>Twitter</h5>
+              <h5>LinkedIn</h5>
+              <h5>Blog</h5>
+            </div>
+          </div>
+          <div className="header-top-section-image-container">
+            <img
+              src="https://res.cloudinary.com/dgwvbd9ki/image/upload/v1708695526/portfolio/edo-logo_jdaxxe.png"
+              alt="edo-cool-image"
+            />
+          </div>
+        </div>
+        <div className="bottom-section">
+          {projects.map((project, index) => (
+            <div className="project-card" key={index}>
+              <div className="project-card-header">
+                <div className="project-logo-container">
+                  <img
+                    className="project-logo"
+                    src={project.logoUrl}
+                    alt="project-logo"
+                  />
+                </div>
+                <h4 className="project-name">{project.name}</h4>
+              </div>
+              <div className="project-card-video-container">
+                <video
+                  className="project-video"
+                  src={project.videoUrl}
+                  alt="video-url"
+                  autoPlay
+                  loop
+                ></video>
+              </div>
+              <p className="project-description">{project.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
